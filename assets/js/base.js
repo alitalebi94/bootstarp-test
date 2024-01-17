5ba695681feb15053ac383f3dd1d59a426f57ecb
ali talebi <a.talebi73@aut.ac.ir>
2024-01-17 19:46:41 +0330

    change card  footer style

--- assets/js/base.js
+++ assets/js/base.js
@@ -14,13 +14,15 @@ function adviceSelection(el){
         let container = document.getElementById('services');
         items.forEach(element => {
             container.innerHTML = container.innerHTML +
-                    '<div class="col-12 col-md-6 col-lg-3"><div class="card m-4"> <img class="p-relative" src="assets/img/services01.jpg" width="100%" height="300px" alt=""><div class="p-absolute pt-5 mt-5 text-center service-card-body w-100">'+
+                    '<div class="col-12 col-md-6 col-lg-3 "><div class="card m-4"><div class="p-absolute card-inner-boader"></div> <img class="p-relative" src="assets/img/services01.jpg" width="100%" height="300px" alt=""><div class="p-absolute pt-5 mt-5 text-center service-card-body w-100">'+
                     '<h2 class="text-center white bold">'+element.label+'</h2>'+
                     '<p class="text-right white px-2 mt-4">'+element.discrip+'</p></div></img>'+              
-                    '<div class="card-footer">'+
-                        '<h5 class="text-center">'+element.name+'</h5>'+
-                        '<p class="service-card-footer mt-4"><span class="mt-2">'+element.value+'</span><button class="btn btn-small btn-outline-light">جزییات</button><span class="mt-2">'+element.date+'</span></p>'+
-                    '</div></div></div>'
+                    '<div class="card-footer p-relative">'+
+                        '<img src="assets/img/card-footer.jpeg" width="100%" height="auto" alt="">'+
+                        '<div class="p-absolute card-footer-content w-100">'+
+                        '<h6 class="text-center mt-2">'+element.name+'</h6>'+
+                            '<p class="service-card-footer mx-4"><span class="mt-2">'+element.value+'</span><button class="btn btn-small btn-outline-light">جزییات</button><span class="mt-2">'+element.date+'</span></p>'+
+                    '</div></img></div></div>'
         });
         subAdvice.innerHTML=subAdvice.innerHTML+' <option value="1">پیشنهاد 1</option>';
     }else if(el == 2){
